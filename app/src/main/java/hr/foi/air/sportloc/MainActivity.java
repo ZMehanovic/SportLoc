c4318137e8bb262a11cd4cc918ac7c4158e303a2
gabilic <gabriel.ilic@gmail.com>
2017-10-28 20:11:57 +0200

    Kreirano korisničko sučelje za početnu aktivnost (aktivnost za prijavu korisnika).

--- app/src/main/java/hr/foi/air/sportloc/MainActivity.java
+++ app/src/main/java/hr/foi/air/sportloc/MainActivity.java
@@ -1,7 +1,10 @@
 package hr.foi.air.sportloc;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        Typeface tfRoboto = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Bold.ttf");
+        TextView tvLogin = (TextView) findViewById(R.id.tvLogin);
+        Button btnAccount = (Button) findViewById(R.id.btnAccount);
+        tvLogin.setTypeface(tfRoboto);
+        btnAccount.setTypeface(tfRoboto);
     }
 }
