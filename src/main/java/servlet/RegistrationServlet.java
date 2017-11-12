dda403e596052fb99c21c75e7155f924c28b743e
ZMehanovic <zlamehano@foi.hr>
2017-11-12 22:27:30 +0100

    Get method changed to post

--- src/main/java/servlet/RegistrationServlet.java
+++ src/main/java/servlet/RegistrationServlet.java
@@ -22,7 +22,7 @@ public class RegistrationServlet extends HttpServletHelper {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		Gson gson = new Gson();
 		JSONObject result = new JSONObject();
