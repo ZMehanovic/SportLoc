c7778df13872673063374ae07408006fd2522649
vglozinic <vglozinic@gmail.com>
2017-11-16 01:42:50 +0100

    Napravljena logika za tipkovnicu i focuse
    Na Login Activity napravljene su metode hideSoftKeyboard, changeFocus i
    setupUI koje su prilikom klika na unfocused element zadužene za uklanjanje focusa i skrivanje tipkovnice sa elementa koji je trenutno u focusu.

--- app/src/main/java/hr/foi/air/sportloc/LoginActivity.java
+++ app/src/main/java/hr/foi/air/sportloc/LoginActivity.java
@@ -1,14 +1,20 @@
 package hr.foi.air.sportloc;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -36,6 +42,9 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.tvRegister)
     TextView tvRegister;
 
+    @BindView(R.id.layout)
+    View layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +53,41 @@ public class LoginActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
         ButterKnife.bind(this);
+        setupUI(layout);
         createDesign();
+    }
+
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        if(activity.getCurrentFocus() != null) {
+            inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+        }
+    }
+
+    public static void changeFocus(View view) {
+        view.setFocusable(true);
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
+    }
+
+    public void setupUI(View view) {
+        List<View> excludedViews = Arrays.asList((View)btnLogin, tvRegister);
+        if(!(view instanceof EditText || excludedViews.contains(view))) {
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    hideSoftKeyboard(LoginActivity.this);
+                    changeFocus(layout);
+                }
+            });
+        }
+
+        if(view instanceof ViewGroup) {
+            for(int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                View innerView = ((ViewGroup) view).getChildAt(i);
+                setupUI(innerView);
+            }
+        }
     }
 
     public void createDesign() {
@@ -59,6 +102,8 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.btnLogin)
     public void loginUserListener(View view) {
+        hideSoftKeyboard(LoginActivity.this);
+        changeFocus(layout);
         String username = txtUsername.getText().toString();
         String password = txtPassword.getText().toString();
         if(checkFieldsEmpty(username, password)) {
@@ -112,6 +157,8 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.tvRegister)
     public void openRegistrationActivity(View view) {
+        hideSoftKeyboard(LoginActivity.this);
+        changeFocus(layout);
         Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
         startActivity(intent);
     }
