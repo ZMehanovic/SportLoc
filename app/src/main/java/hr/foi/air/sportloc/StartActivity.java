f5657887eac7f9b59c5dbd54c2aed855d87dd5da
Vedran Gložinić <vglozinic@gmail.com>
2017-12-05 21:15:49 +0100

    Izrada Start aktivnosti
    Izradio sam novu aktivnost pod nazivom StartActivity u kojoj su napravljena dva gumba koja će voditi na popis događaja i na kreiranje novog događaja. U pozadinu je stavljena slika i logo aplikacije, te su sve one dodane u resurse modula app. Vezano uz to, dodao sam dva nova stringa i xml resurs btn_general_bordered koji opisuje dizajn gumba na toj aktivnosti.

---
+++ app/src/main/java/hr/foi/air/sportloc/StartActivity.java
@@ -0,0 +1,16 @@
+package hr.foi.air.sportloc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+    }
+}
