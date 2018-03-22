320970fc6b4bb1656ea42b868c2107d83659f086
gabilic <gabriel.ilic@gmail.com>
2018-03-22 22:04:36 +0100

    Kreirana aktivnost za prikaz popisa događaja Kreirana je aktivnost za prikaz popisa događaja 'EventListActivity' s pripadajućim layout-om. Za popis događaja će se koristiti 'RecyclerView'. Također, implementirana je logika za klik na gumb 'Pronađi' u aktivnosti 'StartActivity' koja prilikom klika pokreće aktivnost 'EventListActivity'.

---
+++ app/src/main/java/hr/foi/air/sportloc/EventListActivity.java
@@ -0,0 +1,26 @@
+package hr.foi.air.sportloc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class EventListActivity extends AppCompatActivity {
+    @BindView(R.id.rcvEventList)
+    RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_list);
+        ButterKnife.bind(this);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+    }
+}
--- app/src/main/java/hr/foi/air/sportloc/StartActivity.java
+++ app/src/main/java/hr/foi/air/sportloc/StartActivity.java
@@ -3,7 +3,11 @@ package hr.foi.air.sportloc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -14,11 +18,18 @@ public class StartActivity extends AppCompatActivity {
         if(getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(LoginActivity.EXTRA_MESSAGE);
         if(message != null && !message.isEmpty()) {
             Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
         }
+    }
+
+    @OnClick(R.id.btnFind)
+    public void openEventListActivity(View view) {
+        Intent intent = new Intent(StartActivity.this, EventListActivity.class);
+        startActivity(intent);
     }
 }
