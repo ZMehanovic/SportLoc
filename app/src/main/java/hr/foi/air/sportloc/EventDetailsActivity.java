d98195e743b32481f4fc1bff1ef17867f1c13f74
Vedran Gložinić <vglozinic@gmail.com>
2018-03-28 00:59:05 +0200

    Napravljen dizajn pregleda događaja Izrađen je vizualni dizajn i xml file za pregled događaja. U tome se vide detalji o događaju. U strings.xml dodani stringovi, napravljen dizajn btn_disabled za onesposobljeni gumb i dodana ikona za kvačicu.

---
+++ app/src/main/java/hr/foi/air/sportloc/EventDetailsActivity.java
@@ -0,0 +1,18 @@
+package hr.foi.air.sportloc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class EventDetailsActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_details);
+    }
+}
