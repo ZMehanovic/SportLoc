9ee565c1369762e59f67902414ed939848c1ffd0
gabilic <gabriel.ilic@gmail.com>
2018-03-30 04:11:21 +0200

    Kreirana klasa 'EventBean'
    U modul 'data' unutar paketa 'beans' je dodana klasa 'EventBean' koja će se koristiti za rad s podacima vezanim uz sportske događaje (keiranje novih događaja, dohvaćanje liste događaja itd.).

---
+++ data/src/main/java/hr/foi/air/data/beans/EventBean.java
@@ -0,0 +1,110 @@
+package hr.foi.air.data.beans;
+
+/**
+ * Created by Gabriel on 30.3.2018..
+ */
+
+public class EventBean {
+    private int currentCapacity;
+    private int maxCapacity;
+    private boolean openEvent;
+    private String creatorUserName;
+    private String title;
+    private String startTime;
+    private String endTime;
+    private String description;
+    private String address;
+    private String sport;
+    private String location;
+
+    public EventBean() {
+    }
+
+    public int getCurrentCapacity() {
+        return currentCapacity;
+    }
+
+    public void setCurrentCapacity(int currentCapacity) {
+        this.currentCapacity = currentCapacity;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public boolean isOpenEvent() {
+        return openEvent;
+    }
+
+    public void setOpenEvent(boolean openEvent) {
+        this.openEvent = openEvent;
+    }
+
+    public String getCreatorUserName() {
+        return creatorUserName;
+    }
+
+    public void setCreatorUserName(String creatorUserName) {
+        this.creatorUserName = creatorUserName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
