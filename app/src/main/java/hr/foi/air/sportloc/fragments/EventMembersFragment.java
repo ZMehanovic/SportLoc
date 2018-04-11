549318e6e28ae31deaf42fc3e1fc822b814636eb
ZMehanovic <zlamehano@foi.hr>
2018-04-11 23:25:07 +0200

    Added tabs to event details
    Event details will show event information in one tab and particitpants in another, so now there are created initial fragmets for each, event members fragment is currently blank.

---
+++ app/src/main/java/hr/foi/air/sportloc/fragments/EventMembersFragment.java
@@ -0,0 +1,36 @@
+package hr.foi.air.sportloc.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import hr.foi.air.sportloc.R;
+
+public class EventMembersFragment extends Fragment {
+
+
+    @BindView(R.id.tvTest)
+    TextView tvTest;
+    Unbinder unbinder;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_event_members, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
